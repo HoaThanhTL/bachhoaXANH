7ec59e2a91f2953a53b5c504419d67998328c4e7
HoaThanhTL <vominhthuc01051993@gmail.com>
2024-08-04 11:09:32 +0700

    b5 done

--- fe/bachhoaxanh/src/pages/users/theme/header/index.js
+++ fe/bachhoaxanh/src/pages/users/theme/header/index.js
@@ -1,10 +1,54 @@
-
+import {AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineTwitter, AiOutlineUser, AiOutlineMail} from "react-icons/ai";
 import {memo } from "react";
+import { Link } from 'react-router-dom';
 import "./style.scss"
+import { formatter } from "utils/fomater";
  const  Header = () => {
     return (
-    <div className="heard__top">
-        <div className="container">Header</div>
+    <div className="header__top">
+        <div className="container">
+            <div className="row">
+                <div className="col-6 header__top_left">
+                    <ul>
+                        <li>
+                            <AiOutlineMail />
+                            hello@gmail.com
+                        </li>
+                        <li>Miễn phí đơn từ {formatter(200000)}</li>
+                    </ul>
+                </div>
+                <div className="col-6 header__top_right">
+                    <ul>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineFacebook />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineInstagram />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineLinkedin />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineTwitter />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={""}>
+                                <AiOutlineUser />
+                            </Link>
+                            <span>Đăng nhập</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
     );
     
