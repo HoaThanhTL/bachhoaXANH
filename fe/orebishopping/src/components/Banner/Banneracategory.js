3008225e33ca9ebfa007390b7987781153d2ba1d
HoaThanhTL <vominhthuc01051993@gmail.com>
2024-11-05 23:26:57 +0700

    Mô tả thay đổi

---
+++ fe/orebishopping/src/components/Banner/Banneracategory.js
@@ -0,0 +1,1 @@
+//render theo list category
---
+++ fe/orebishopping/src/components/home/data/categoriesData.js
@@ -0,0 +1,101 @@
+const categoriesData = [
+    {
+      _id: "cat001",
+      name: "Hàng tươi sống",
+      listImg: [
+        "https://example.com/images/fresh1.jpg",
+        "https://example.com/images/fresh2.jpg",
+        "https://example.com/images/fresh3.jpg",
+      ],
+      subcategories: [
+        {
+          _id: "subcat001",
+          name: "Rau củ",
+          listImg: [
+            "https://example.com/images/vegetables1.jpg",
+            "https://example.com/images/vegetables2.jpg",
+          ],
+        },
+        {
+          _id: "subcat002",
+          name: "Trái cây",
+          listImg: [
+            "https://example.com/images/fruits1.jpg",
+            "https://example.com/images/fruits2.jpg",
+          ],
+        },
+        {
+          _id: "subcat003",
+          name: "Thịt cá",
+          listImg: [
+            "https://example.com/images/meat1.jpg",
+            "https://example.com/images/meat2.jpg",
+          ],
+        },
+      ],
+    },
+    {
+      _id: "cat002",
+      name: "Đồ gia dụng",
+      listImg: [
+        "https://example.com/images/home1.jpg",
+        "https://example.com/images/home2.jpg",
+      ],
+      subcategories: [
+        {
+          _id: "subcat004",
+          name: "Đồ bếp",
+          listImg: [
+            "https://example.com/images/kitchen1.jpg",
+            "https://example.com/images/kitchen2.jpg",
+          ],
+        },
+        {
+          _id: "subcat005",
+          name: "Thiết bị vệ sinh",
+          listImg: [
+            "https://example.com/images/cleaning1.jpg",
+            "https://example.com/images/cleaning2.jpg",
+          ],
+        },
+      ],
+    },
+    {
+      _id: "cat003",
+      name: "Thời trang",
+      listImg: [
+        "https://example.com/images/fashion1.jpg",
+        "https://example.com/images/fashion2.jpg",
+        "https://example.com/images/fashion3.jpg",
+      ],
+      subcategories: [
+        {
+          _id: "subcat006",
+          name: "Quần áo nam",
+          listImg: [
+            "https://example.com/images/mens_clothing1.jpg",
+            "https://example.com/images/mens_clothing2.jpg",
+          ],
+        },
+        {
+          _id: "subcat007",
+          name: "Quần áo nữ",
+          listImg: [
+            "https://example.com/images/womens_clothing1.jpg",
+            "https://example.com/images/womens_clothing2.jpg",
+          ],
+        },
+        {
+          _id: "subcat008",
+          name: "Phụ kiện",
+          listImg: [
+            "https://example.com/images/accessories1.jpg",
+            "https://example.com/images/accessories2.jpg",
+          ],
+        },
+      ],
+    },
+  ];
+  
+  export default categoriesData;
+  
